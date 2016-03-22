e92ed43800612febf71da62ccdc2744c3d1893e6
Aliaksandr Shylau <alex.shilov.by@gmail.com>
2016-03-22 16:20:51 +0300

    Add banks query spec

--- spec/bank_exchange_api_spec.rb
+++ spec/bank_exchange_api_spec.rb
@@ -15,8 +15,31 @@ RSpec.describe BankExchangeApi do
       it { expect(subject.ping).to be(true) }
     end
 
-    context 'offline',  vcr: {cassette_name: 'ping_offline'} do
+    context 'offline', vcr: {cassette_name: 'ping_offline'} do
       it { expect(subject.ping).to be(false) }
     end
+  end
+
+  describe '#banks', vcr: {cassette_name: 'banks'} do
+    let(:json) do
+      subject.banks(currencies: :USD).json
+    end
+
+    let(:data) do
+      [{"swift"=>"XXXXXXXX",
+        "name"=>"Board of Governors of the Federal Reserve System",
+        "country"=>"US",
+        "currency"=>"USD",
+        "website"=>"http://www.federalreserve.gov"}]
+    end
+
+    let(:params) do
+      {"countries"=>[], "currencies"=>["USD"]}
+    end
+
+    it {
+      expect(json.data).to eq(data)
+      expect(json.params).to eq(params)
+    }
   end
 end
